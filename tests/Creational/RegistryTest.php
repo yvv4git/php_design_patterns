808ca7d7a55a8dd9a13ed40326c8db5d471ca107
yvv <yvv4git>
2020-06-05 17:21:58 +0300

    Add test to creatioanl pattern Registry

---
+++ tests/Creational/RegistryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests;
+
+use DesignPatterns\Registry\Priora;
+use DesignPatterns\Registry\ProductRegistry;
+use DesignPatterns\Registry\Vesta;
+use PHPUnit\Framework\TestCase;
+
+class RegistryTest extends TestCase
+{
+    private static $registry;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$registry = new ProductRegistry;
+    }
+
+    public function testPriora()
+    {
+        self::$registry->set(1, new Priora);
+        $product = self::$registry->get(1);
+        
+        $this->assertInstanceOf(Priora::class, $product);
+        $this->assertSame('Priora', $product->getProductName());
+    }
+
+    public function testVesta()
+    {
+        self::$registry->set(2, new Vesta);
+        $product = self::$registry->get(2);
+        
+        $this->assertInstanceOf(Vesta::class, $product);
+        $this->assertSame('Vesta', $product->getProductName());
+    }
+}
