fd773c1c2bd977ddecc926d6b951844549ab9d5b
yvv <yvv4git>
2020-06-05 18:04:39 +0300

    Refactoring

--- tests/Creational/PrototypeTest.php
+++ tests/Creational/PrototypeTest.php
@@ -10,6 +10,8 @@ class PrototypeTest extends TestCase
 {
     public function testCloneProduct()
     {
+        // new Priora - наш изначальный объект, который может иметь
+        // массу полезных свойств, которые необходимо скопировать.
         $carFactory = new Factory(new Priora);
         $firstCar = $carFactory->getCar();
         $secondCar = $carFactory->getCar();
@@ -17,9 +19,12 @@ class PrototypeTest extends TestCase
         $this->assertSame('Priora', $firstCar->getCarName());
         $this->assertSame('Priora', $secondCar->getCarName());
 
+        // Еслиб это был один и тот же объект, то в итоге его название стало бы 'Priora-v2'
         $firstCar->setCarName('Priora-v1');
         $secondCar->setCarName('Priora-v2');
 
+        // Тесты показывают, что это ссылки на разные объекты. 
+        // Следовательно клонирование отработало.
         $this->assertSame('Priora-v1', $firstCar->getCarName());
         $this->assertSame('Priora-v2', $secondCar->getCarName());
     }
